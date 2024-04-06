91d7a5ec4f82af6c74454fb709f7d5d60722419e
NinaAbeyratne <150835212+NinaAbeyratne@users.noreply.github.com>
2024-04-06 16:50:13 +0530

    Image preprocess for both models and model code updated

--- app.py
+++ app.py
@@ -21,6 +21,48 @@ classification_model = load_model('/Users/new/Downloads/ResNet152V2.h5', compile
 def index():
     return render_template("index.html")
 
+
+# Image path
+image_path_folder = '/Users/new/Downloads/ratsnake-basking-on-forest_med_hr.jpeg'
+nisith_img = '/Users/new/Downloads/ratsnake-basking-on-forest_med_hr.jpeg'
+
+
+# Component 1 image preprocess
+def identification_image_preprocess(image_path_folder):
+    img = cv2.imread(image_path_folder)
+    img = cv2.resize(img, (224, 224))
+    img = np.expand_dims(img, axis=0)
+    return img
+
+
+# Component 2 image preprocess
+def classification_image_preprocess(image_path):
+    img = image.load_img(nisith_img, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+
+# Component 1 model
+nina_model_preprocess_image = identification_image_preprocess(nisith_img)
+predictions = identification_model.predict(nina_model_preprocess_image)
+print(predictions)
+predicted_class = np.argmax(predictions)
+print(f"Predicted Class Nina: {predicted_class}")
+
+# Component 2 model
+if predicted_class == 1:
+    print("Nisitha")
+    nisitha_image = classification_image_preprocess(image_path_folder)
+
+    y = classification_model.predict(nisitha_image)
+    preds = np.argmax(y)
+    index = ["Common Rat Snake", "Russel's Viper", "Forsten's Cat Snake", "Green Pit Viper"]
+    print("The classified Animal is : " + str(index[preds]))
+    print("predictions", y)
+
+
 def is_snake(image_path):
     img = image.load_img(image_path, target_size=(224, 224))
     x = image.img_to_array(img)
